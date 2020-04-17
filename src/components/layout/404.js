97dd8ed549267f1900a10fe4906e79998681c2e4
marinelcirstea <marinel.cirstea@gmail.com>
2020-04-17 23:10:52 +0100

    set up boilerplate for future auth features

---
+++ src/components/layout/404.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <h1 className="x-large text-primary">You seem to be lost, friend..</h1>
+      <p className="large">Looks like this page does not exist</p>
+    </Fragment>
+  );
+};
+
+export default NotFound;
